ec6c3cb14fc6b8fe6d80601794d091d24375ebb8
Nataliia Sychevska <petriwna43@gmail.com>
2022-01-31 17:47:11 +0200

    get one photo

--- 14_homework_events/src/pages/Main/index.ts
+++ 14_homework_events/src/pages/Main/index.ts
@@ -1,2 +1,21 @@
 import "./styles.scss"
+function init() {
+  getImage();
+}
+function getImage(){
+  fetch('https://jsonplaceholder.typicode.com/photos/1')
+    .then((response) => response.json())
+    .then((json) => renderImage(json));
+}
 
+function renderImage(json: any) {
+  const imgContainer = document.getElementById('item');
+
+  imgContainer.innerHTML = `<img src="${json.url}" class="gallery__img"/>`
+  console.log(json)
+  return imgContainer;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  init();
+});
