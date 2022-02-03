4587f4c6e39d03b69be00c98078a16ed8a4b478a
Nataliia Sychevska <petriwna43@gmail.com>
2022-02-03 20:32:02 +0200

    add big image start load

--- 14_homework_events/src/pages/ts/Gallery.ts
+++ 14_homework_events/src/pages/ts/Gallery.ts
@@ -4,9 +4,11 @@ import {renderBigPhoto} from "./renderBigPhoto";
 
 export class Gallery implements IGallery{
   getImages() {
+    const bigPhoto = document.getElementById('big-photo');
     return fetch('https://jsonplaceholder.typicode.com/photos')
       .then(response => response.json())
       .then(data => {
+        bigPhoto.setAttribute('src', `${data[0].url}`)
         const images = data.slice(0, 20);
           images.forEach(img => renderImage(img.url, img.id));
       });
