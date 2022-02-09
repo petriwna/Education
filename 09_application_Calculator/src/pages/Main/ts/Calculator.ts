84abbc4102bf63b9d33ba178a762b1fa856eb257
Nataliia Sychevska <petriwna43@gmail.com>
2022-02-09 23:48:07 +0200

    add new functional

--- 09_application_Calculator/src/pages/Main/ts/Calculator.ts
+++ 09_application_Calculator/src/pages/Main/ts/Calculator.ts
@@ -14,7 +14,7 @@ export class Calculator implements CalcInterface {
     this.operator = null;
   }
 
-  calculation(a, b, operator){
+  calculation(a, b, operator) {
     if (operator === '+') {
       return this.sum(a, b);
     } else if (operator === '-') {
@@ -23,67 +23,13 @@ export class Calculator implements CalcInterface {
       return this.multiplication(a, b);
     } else if (operator === '/') {
       return this.division(a, b);
+    } else if (operator === 'xy'){
+      return this.pow(a, b);
     }
     return b;
   }
 
-  constantsToPower(): number {
-    return Math.pow(Math.E, this.a);
-  }
-
-  cube(): number {
-    return Math.pow(this.a, 3);
-  }
-
-  division(a: number, b: number): number {
-    return a / b;
-  }
-
-  multiplication(a: number, b: number): number {
-    return a * b;
-  }
-
-  power(b: number): number {
-    return Math.pow(this.a, b);
-  }
-
-  squared(): number {
-    return Math.sqrt(this.a);
-  }
-
-  subtraction(a: number, b: number): number {
-    return a - b;
-  }
-
-  sum(a: number, b: number): number {
-    return a + b;
-  }
-
-  tenToPower(): number {
-    return Math.pow(10, this.a);
-  }
-
-  square(): number {
-    return Math.pow(this.a, 2);
-  }
-
-  oneDivision(): number {
-    return 1 / this.a;
-  }
-
-  pi(): number {
-    return Math.PI;
-  }
-
-  constant(): number {
-    return Math.E;
-  }
-
-  multipToTenPower(b: number): number {
-    return this.a * (Math.pow(10, b));
-  }
-
-  clearAll(): void{
+  clearAll(): void {
     this.displayValue = '0';
     this.firstOperand = null;
     this.waitingForSecondOperand = false;
@@ -97,7 +43,22 @@ export class Calculator implements CalcInterface {
       case 'x':
       case '/':
       case '=':
+      case 'xy':
         this.handleOperator(event.target.value);
+        break;
+      case 'П':
+      case '%':
+      case '+/-':
+      case 'x!':
+      case 'sin':
+      case 'cos':
+      case 'tan':
+      case 'log':
+      case 'x2':
+      case 'x3':
+      case 'ex':
+      case '10x':
+        this.handlerFn(event.target.value);
         break;
       case '.':
         this.inputDecimal(event.target.value);
@@ -113,37 +74,76 @@ export class Calculator implements CalcInterface {
     updateDisplay(this.displayValue);
   }
 
-  inputDecimal(dot) {
-    if (this.waitingForSecondOperand === true){
-      this.displayValue = '0';
-      this.waitingForSecondOperand = false;
-      return;
-    }
-    if (!this.displayValue.includes(dot)) {
-      this.displayValue = this.displayValue + dot;
-    }
-  }
-
   handleOperator(nextOperator) {
     const inputValue = parseFloat(this.displayValue);
 
     if (this.operator && this.waitingForSecondOperand) {
       this.operator = nextOperator;
-      console.log(nextOperator);
       return;
     }
 
     if (this.firstOperand === null && !isNaN(inputValue)) {
       this.firstOperand = inputValue;
-    } else if (this.operator){
+    } else if (this.operator) {
       const result = this.calculation(this.firstOperand, inputValue, this.operator)
-      this.displayValue = String(result);
+      this.displayValue = `${parseFloat(result.toFixed(7))}`;
       this.firstOperand = result;
     }
 
     this.waitingForSecondOperand = true;
     this.operator = nextOperator;
-    console.log(this.displayValue, this.waitingForSecondOperand, this.operator)
+  }
+
+  handlerFn(fn) {
+    const currentInput = parseFloat(this.displayValue);
+    let result;
+
+    switch (fn) {
+      case 'П':
+        result = currentInput === 0 ? this.pi() : currentInput * this.pi();
+        break;
+      case '%':
+        result = currentInput / 100;
+        break;
+      case '+/-':
+        result = currentInput * -1;
+        break;
+      case 'x!':
+        result = this.factorial(currentInput);
+        break;
+      case 'sin':
+        result = this.sin(currentInput);
+        break;
+      case 'cos':
+        result = this.cos(currentInput);
+        break;
+      case 'tan':
+        result = this.tan(currentInput);
+        break;
+      case 'log':
+        result = this.log(currentInput);
+        break;
+      case 'x2':
+        result = this.powTwo(currentInput);
+        break;
+      case 'x3':
+        result = this.cube(currentInput);
+        break;
+      case 'ex':
+        result = this.constantsToPower(currentInput);
+        break;
+      case '10x':
+        result = this.tenToPower(currentInput);
+        break;
+      default:
+        result = 0;
+    }
+
+    this.displayValue = `${parseFloat(result.toFixed(10))}`;
+
+    if (this.waitingForSecondOperand) {
+      this.waitingForSecondOperand = false;
+    }
   }
 
   inputDisplay(digit) {
@@ -155,4 +155,106 @@ export class Calculator implements CalcInterface {
       this.displayValue = this.displayValue === '0' ? digit : this.displayValue + digit;
     }
   }
+
+  inputDecimal(dot) {
+    if (this.waitingForSecondOperand === true) {
+      this.displayValue = '0';
+      this.waitingForSecondOperand = false;
+      return;
+    }
+    if (!this.displayValue.includes(dot)) {
+      this.displayValue = this.displayValue + dot;
+    }
+  }
+
+  factorial(a) {
+    if (!Number.isInteger(a) || a < 0) {
+      alert('Factorial is only defined for positive integers');
+      return a;
+    }
+    if (a === 0 || a === 1) return 1;
+
+    for (let i = a - 1; i >= 1; i--) {
+      a = a * i;
+    }
+    return a;
+  }
+
+  sin(a) {
+    return Math.sin(a * (this.pi() / 180));
+  }
+
+  cos(a) {
+    return Math.cos(a * (this.pi() / 180));
+  }
+
+  tan(a) {
+    return Math.tan(a * (this.pi() / 180));
+  }
+
+  log(a) {
+    return Math.log10(a);
+  }
+
+  powTwo(a: number): number {
+    return Math.pow(a, 2);
+  }
+
+  cube(a): number {
+    return Math.pow(a, 3);
+  }
+
+  pow(a: number, b: number): number{
+    return Math.pow(a, b);
+  }
+
+  constantsToPower(a): number {
+    return Math.pow(Math.E, a);
+  }
+
+  tenToPower(a): number {
+    return Math.pow(10, a);
+  }
+
+  division(a: number, b: number): number {
+    return a / b;
+  }
+
+  multiplication(a: number, b: number): number {
+    return a * b;
+  }
+
+  squared(a): number {
+    return Math.sqrt(a);
+  }
+
+  subtraction(a: number, b: number): number {
+    return a - b;
+  }
+
+  sum(a: number, b: number): number {
+    return a + b;
+  }
+
+
+  square(a): number {
+    return Math.pow(a, 2);
+  }
+
+  oneDivision(a): number {
+    return 1 / a;
+  }
+
+  pi(): number {
+    return Math.PI;
+  }
+
+  constant(): number {
+    return Math.E;
+  }
+
+  multipToTenPower(a, b: number): number {
+    return a * (Math.pow(10, b));
+  }
+
 }
