76e5536e76357bdd0c9ccb512ae9572108102d5a
Nataliia Sychevska <petriwna43@gmail.com>
2022-02-14 01:27:46 +0200

    add move circle

--- 11_app_Painter_app_Circles/app_Circles/src/pages/ts/Circles.ts
+++ 11_app_Painter_app_Circles/app_Circles/src/pages/ts/Circles.ts
@@ -17,7 +17,8 @@ export class Circles {
     const svg = this.createSvgElement('svg');
     svg.setAttribute('width', String((circle.radius) * 2));
     svg.setAttribute('height', String((circle.radius) * 2));
-    svg.style = `top: ${position.y}px; left: ${position.x}px;`;
+    svg.style.left = `${position.x}px`;
+    svg.style.top= `${position.y}px`;
     svg.setAttributeNS(null, 'fill', this.createCircle().color);
 
     const circleSvg = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
@@ -25,7 +26,52 @@ export class Circles {
     circleSvg.setAttributeNS(null, 'cx', String(circle.radius));
     circleSvg.setAttributeNS(null, 'cy', String(circle.radius));
     svg.appendChild(circleSvg);
+
     this.container.appendChild(svg);
+    let nowLeft = svg.getBoundingClientRect().left + (circle.speed / 1000) * circle.dx;
+    console.log(nowLeft);
+    // Получить верхнее значение текущего поля
+    let nowTop = svg.getBoundingClientRect().top + (circle.speed / 1000) * circle.dy;
+    console.log(nowTop);
+    // Получить максимальное расстояние перемещения оси х шара
+    const maxWidth = this.container.offsetWidth - svg.getBoundingClientRect().width;
+    console.log(maxWidth);
+    // Получаем максимальное расстояние перемещения по оси Y шара
+    const maxHeight = this.container.offsetHeight - svg.getBoundingClientRect().height;
+    console.log(maxHeight);
+
+    // Таймер входит в цикл
+    setInterval(function () {
+      console.log(nowLeft);
+      // Изменить левое значение шара
+      nowLeft += circle.speed;
+      // Когда мяч достигает крайнего правого угла, полученная скорость должна стать отрицательной
+      if (nowLeft >= maxWidth) {
+        circle.speed  = -circle.speed ;
+      }
+      // Когда мяч снова достигнет крайнего левого угла, полученная скорость будет положительной
+      if (nowLeft <= 0) {
+        circle.speed  = parseInt(String(Math.random() * 10 + 1));
+      }
+      // Устанавливаем левое значение для мяча
+      svg.style.left = nowLeft + "px";
+
+      // Изменить верхнее значение шара
+      nowTop += circle.speed ;
+      // Когда мяч достигает дна, скорость становится отрицательной
+      if (nowTop >= maxHeight) {
+        circle.speed  = parseInt(String(Math.random() * 10 + 1));
+        circle.speed  = -circle.speed ;
+      }
+      // Когда мяч снова достигает верхнего, скорость становится положительной
+      if (nowTop <= 0) {
+        circle.speed  = parseInt(String(Math.random() * 10 + 1));
+      }
+      // Установить верхнее значение для шара
+      svg.style.top = nowTop + "px";
+
+    }, 30);
+
   }
 
   getMousePosition(element, event){
