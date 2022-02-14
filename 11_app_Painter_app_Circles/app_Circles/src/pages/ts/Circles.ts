fed02266a1f07ebe359783be65a60295e27ebedb
Nataliia Sychevska <petriwna43@gmail.com>
2022-02-14 02:45:00 +0200

    finish

--- 11_app_Painter_app_Circles/app_Circles/src/pages/ts/Circles.ts
+++ 11_app_Painter_app_Circles/app_Circles/src/pages/ts/Circles.ts
@@ -1,4 +1,4 @@
-import {MAX_RADIUS, MAX_SPEED, MIN_RADIUS, MIN_SPEED} from "./constants";
+import {FULL_HEIGHT, FULL_WIDTH, MAX_RADIUS, MAX_SPEED, MIN_RADIUS, MIN_SPEED} from "./constants";
 
 export class Circles {
   container: HTMLElement;
@@ -18,8 +18,9 @@ export class Circles {
     svg.setAttribute('width', String((circle.radius) * 2));
     svg.setAttribute('height', String((circle.radius) * 2));
     svg.style.left = `${position.x}px`;
-    svg.style.top= `${position.y}px`;
+    svg.style.top = `${position.y}px`;
     svg.setAttributeNS(null, 'fill', this.createCircle().color);
+    this.container.appendChild(svg);
 
     const circleSvg = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
     circleSvg.setAttributeNS(null, 'r', String(circle.radius));
@@ -27,35 +28,28 @@ export class Circles {
     circleSvg.setAttributeNS(null, 'cy', String(circle.radius));
     svg.appendChild(circleSvg);
 
-    this.container.appendChild(svg);
-    let nowLeft = svg.getBoundingClientRect().left + (circle.speed / 1000) * circle.dx;
-    console.log(nowLeft);
-    let nowTop = svg.getBoundingClientRect().top + (circle.speed / 1000) * circle.dy;
-    console.log(nowTop);
-    const maxWidth = this.container.offsetWidth - svg.getBoundingClientRect().width;
-    console.log(maxWidth);
-    const maxHeight = this.container.offsetHeight - svg.getBoundingClientRect().height;
-    console.log(maxHeight);
+    let speed = Math.floor(Math.random() * MAX_SPEED + MIN_SPEED);
+    let nowLeft = svg.getBoundingClientRect().left + (speed / 1000) * circle.dx;
+    let nowTop = svg.getBoundingClientRect().top + (speed / 1000) * circle.dy;
 
-    // Таймер входит в цикл
     setInterval(function () {
       console.log(nowLeft);
-      nowLeft += circle.speed;
-      if (nowLeft >= maxWidth) {
-        circle.speed  = -circle.speed ;
+      nowLeft += speed;
+      if (nowLeft >= FULL_WIDTH) {
+        speed = -speed;
       }
       if (nowLeft <= 0) {
-        circle.speed  = parseInt(String(Math.random() * 10 + 1));
+        speed = Math.floor(Math.random() * MAX_SPEED + MIN_SPEED);
       }
       svg.style.left = nowLeft + "px";
 
-      nowTop += circle.speed ;
-      if (nowTop >= maxHeight) {
-        circle.speed  = parseInt(String(Math.random() * 10 + 1));
-        circle.speed  = -circle.speed ;
+      nowTop += speed;
+      if (nowTop >= FULL_HEIGHT) {
+        speed = Math.floor(Math.random() * MAX_SPEED + MIN_SPEED);
+        speed = -speed;
       }
       if (nowTop <= 0) {
-        circle.speed  = parseInt(String(Math.random() * 10 + 1));
+        speed = Math.floor(Math.random() * MAX_SPEED + MIN_SPEED);
       }
       svg.style.top = nowTop + "px";
 
@@ -63,9 +57,9 @@ export class Circles {
 
   }
 
-  getMousePosition(element, event){
+  getMousePosition(element, event) {
     const rect = this.container.getBoundingClientRect();
-    const { clientX, clientY} = event
+    const {clientX, clientY} = event
     return {
       x: clientX - rect.x,
       y: clientY - rect.y
@@ -77,8 +71,7 @@ export class Circles {
     const angle = Math.random() * 2 * Math.PI;
     const dx = Math.cos(angle);
     const dy = Math.sin(angle);
-    const speed = Math.floor(Math.random() * MAX_SPEED + MIN_SPEED);
     const color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-    return {dx, dy, radius, color, speed};
+    return {dx, dy, radius, color};
   }
 }
