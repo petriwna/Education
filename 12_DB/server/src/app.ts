32e38dbaff3f4e877623e747eea6bda227f19c43
Nataliia Sychevska <petriwna43@gmail.com>
2022-02-14 03:38:52 +0200

    refactor code hw12-DB

--- 12_DB/server/src/app.ts
+++ 12_DB/server/src/app.ts
@@ -1,82 +1,94 @@
-import {MySQL} from "./MySQL";
-const express = require( "express");
+const mysql = require('mysql');
 
-const app = express();
-const port = 3006;
-
-app.listen(port, async () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-    const mySQL = new MySQL({
-        host: "127.0.0.1",
-        user: "root",
-        database: "mybd",
-        password: "Testtest1!"
-    });
-    await mySQL.connection();
-
-    app.get('/person', (req, res) => {
-        mySQL.queryAll('SELECT * FROM person', (rows) => {
-            res.send(rows);
-        });
-    });
-
-    app.get('/task1', (req , res) => {
-        mySQL.queryAll('SELECT COUNT(*) FROM person', (rows) => {
-            res.send(rows);
-        });
-    });
-
-    app.get('/task2', (req , res) => {
-        mySQL.queryAll('SELECT AVG(age) FROM person', (rows) => {
-            res.send(rows);
-        });
-    });
-
-    app.get('/task3', (req , res) => {
-        mySQL.queryAll('SELECT DISTINCT LastName FROM person ORDER BY LastName', (rows) => {
-            res.send(rows);
-        });
-    });
-
-    app.get('/task4', (req , res) => {
-        mySQL.queryAll('SELECT LastName, COUNT(*) FROM person GROUP BY LastName', (rows) => {
-            res.send(rows);
-        });
-    });
-
-    app.get('/task5', (req , res) => {
-        mySQL.queryAll(`SELECT LastName FROM person WHERE LastName LIKE \'_%б%_'`, (rows) => {
-            res.send(rows);
-        });
-    });
-
-    app.get('/task6', (req , res) => {
-        mySQL.queryAll('SELECT * FROM person WHERE Id_Street IS NULL', (rows) => {
-            res.send(rows);
-        });
-    });
-
-    app.get('/task7', (req , res) => {
-        mySQL.queryAll('SELECT person.*FROM person JOIN street ON person.Id_Street = street.ID WHERE street.Name LIKE \'%проспект Правды%\' AND person.Age < 18', (rows) => {
-            res.send(rows);
-        });
-    });
-
-    app.get('/task8', (req , res) => {
-        mySQL.queryAll('SELECT street.ID, street.Name, COUNT(person.ID) FROM street JOIN person ON street.ID = person.Id_Street GROUP BY street.ID, street.Name', (rows) => {
-            res.send(rows);
-        });
-    });
-
-    app.get('/task9', (req , res) => {
-        mySQL.queryAll('SELECT * FROM street WHERE LENGTH(Name) = 6', (rows) => {
-            res.send(rows);
-        });
-    });
-
-    app.get('/task10', (req , res) => {
-        mySQL.queryAll('SELECT street.ID, street.Name FROM street JOIN person ON street.ID = person.Id_Street GROUP BY street.ID, street.Name HAVING COUNT(person.ID) < 3', (rows) => {
-            res.send(rows);
-        });
-    });
+const connect = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    database: "mybd",
+    password: "Testtest1!"
 });
+
+connect.connect((error) => {
+    if (error) {
+        return console.error("Ошибка: " + error.message);
+    } else {
+        console.log("Подключение к серверу MySQL успешно установлено");
+    }
+});
+
+function allPerson(){
+    const query = 'SELECT * FROM person';
+    connect.query(query, (error, res) => {
+        if(!error) console.log(res);
+    });
+}
+allPerson();
+
+function task1(){
+    const query = 'SELECT COUNT(*) FROM person';
+    connect.query(query, (error, res) => {
+        if(!error) console.log(res);
+    });
+}
+
+function task2(){
+    const query = 'SELECT AVG(age) FROM person';
+    connect.query(query, (error, res) => {
+        if(!error) console.log(res);
+    });
+}
+
+function task3(){
+    const query = 'SELECT DISTINCT LastName FROM person ORDER BY LastName';
+    connect.query(query, (error, res) => {
+        if(!error) console.log(res);
+    });
+}
+
+function task4(){
+    const query = 'SELECT LastName, COUNT(*) FROM person GROUP BY LastName';
+    connect.query(query, (error, res) => {
+        if(!error) console.log(res);
+    });
+}
+
+function task5(){
+    const query = `SELECT LastName FROM person WHERE LastName LIKE \'_%б%_'`;
+    connect.query(query, (error, res) => {
+        if(!error) console.log(res);
+    });
+}
+
+function task6(){
+    const query = 'SELECT * FROM person WHERE Id_Street IS NULL';
+    connect.query(query, (error, res) => {
+        if(!error) console.log(res);
+    });
+}
+
+function task7(){
+    const query = 'SELECT person.*FROM person JOIN street ON person.Id_Street = street.ID WHERE street.Name LIKE \'%проспект Правды%\' AND person.Age < 18';
+    connect.query(query, (error, res) => {
+        if(!error) console.log(res);
+    });
+}
+
+function task8(){
+    const query = 'SELECT street.ID, street.Name, COUNT(person.ID) FROM street JOIN person ON street.ID = person.Id_Street GROUP BY street.ID, street.Name';
+    connect.query(query, (error, res) => {
+        if(!error) console.log(res);
+    });
+}
+
+function task9(){
+    const query = 'SELECT * FROM street WHERE LENGTH(Name) = 6';
+    connect.query(query, (error, res) => {
+        if(!error) console.log(res);
+    });
+}
+
+function task10(){
+    const query = 'SELECT street.ID, street.Name FROM street JOIN person ON street.ID = person.Id_Street GROUP BY street.ID, street.Name HAVING COUNT(person.ID) < 3';
+    connect.query(query, (error, res) => {
+        if(!error) console.log(res);
+    });
+}
--- 12_DB/web/src/pages/ts/IPerson.ts
+++
@@ -1,4 +0,0 @@
-export interface IPerson {
-  getTablePerson: () => void
-  renderTablePerson: () => void
-}
